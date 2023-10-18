c637a64ee7bf3720f140bf40d2457fafefe29bc5
BenM4k <bennymakomo@gmail.com>
2023-10-18 12:28:57 +0200

    SMTP Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: 'ENV['GMAIL_USERNAME']',
-    user_name: 'ENV['GMAIL_PASSWORD']',
+    password: 'ENV['GMAIL_PASSWORD']',
   }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
